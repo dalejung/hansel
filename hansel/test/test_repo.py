ca446a79ad62ab3ae045f88e0f12d28d3ec46eb0
Dale Jung <dale@dalejung.com>
2015-06-30 09:33:42 -0400

    added failing tests as reminders

---
+++ hansel/test/test_repo.py
@@ -0,0 +1,5 @@
+def test_repo_persist():
+    assert False, "TODO: Keep track of Repo instances and persist their data via pickle"
+
+def test_repo_aggregate_scaffolding():
+    assert False, "TODO: Let Repo define an aggregate, then type check"
