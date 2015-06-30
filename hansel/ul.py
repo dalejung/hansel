4ddbd76005da8bf4abc902e3362179c764d785f3
Dale Jung <dale@dalejung.com>
2015-06-30 09:37:47 -0400

    UL notes

--- hansel/ul.py
+++ hansel/ul.py
@@ -1,3 +1,11 @@
+"""
+The ubiquitous language module. Basically tools to allow defining a UL, which
+is essentially name => validator that can be applied to method params and
+varialbe names.
+
+Originally the idea was to make this a Service specific feature, but in reality
+a UL instance should be able to wrap arbitrary class / methods
+"""
 from .context_util import WithScope
 
 class UL(object):
