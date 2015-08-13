36f7818827d15ff23ed3e482f26554dbb590737e
Dale Jung <dale@dalejung.com>
2015-08-13 12:28:40 -0400

    travis

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@ from distutils.core import setup
 DISTNAME='hansel'
 FULLVERSION='0.1'
 
-setup(name=DISTNAME,
-      version=FULLVERSION,
-      packages=['hansel']
-      )
+setup(
+    name=DISTNAME,
+    version=FULLVERSION,
+    packages=['hansel'],
+    install_requires = [
+        'astor',
+        'asttools'
+        'earthdragon',
+        'six',
+    ]
+)
