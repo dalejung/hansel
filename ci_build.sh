7ebc27c4783123a33a4f33eba709ff4fa1afdcd4
Dale Jung <dale@dalejung.com>
2016-03-05 04:24:07 -0500

    3.5 needs astor master

--- ci_build.sh
+++ ci_build.sh
@@ -2,6 +2,7 @@
 
 pip instlal numpy
 pip install pandas
+pip install git+https://github.com/berkerpeksag/astor.git
 pip install git+https://github.com/dalejung/asttools
 pip install git+https://github.com/dalejung/earthdragon
 pip install .
