e1145099778057531e37d04667a14bce7332fe90
Dale Jung <dale@dalejung.com>
2016-03-07 10:49:08 -0500

    Moved the typelet filling to earthdragon.typelet

--- hansel/value_object.py
+++ hansel/value_object.py
@@ -2,13 +2,17 @@ from collections import OrderedDict
 
 from six import iteritems, with_metaclass
 
-from earthdragon.typelet import Typelet, gather_typelets, typelet_repr
+from earthdragon.typelet import (
+    Typelet,
+    gather_typelets,
+    typelet_repr,
+    TypeletMeta,
+    inflate,
+    InvalidInitInvocation
+)
 from earthdragon.meta import mro
 
 class IllegalMutation(Exception):
-    pass
-
-class InvalidInitInvocation(Exception):
     pass
 
 def init_wrapper(func):
@@ -19,26 +23,18 @@ def init_wrapper(func):
         self._hansel_locked = True
     return _init
 
-class ValueObjectMeta(type):
+class ValueObjectMeta(TypeletMeta):
     def __prepare__(name, bases):
         mdict = OrderedDict()
         return mdict
 
     def __new__(cls, name, bases, dct):
         if bases:# only run on ValueObject subclass
-            typelets = gather_typelets(dct, bases)
-            dct['_earthdragon_typelets'] = typelets
-
             init = mro(dct, bases, '__init__')
 
             dct['__init__'] = init_wrapper(init)
         return super(ValueObjectMeta, cls).__new__(cls, name, bases, dct)
 
-def fill(obj, filled, name, value):
-    if name in filled:
-        raise InvalidInitInovation("Error")
-    setattr(obj, name, value)
-    filled[name] = True
 
 class ValueObject(metaclass=ValueObjectMeta):
     _hansel_locked = False
@@ -55,22 +51,6 @@ class ValueObject(metaclass=ValueObjectMeta):
 
     def __init__(self, *args, **kwargs):
         """
-        Default __init__. Requires that earthdragon_typelets be specified.
+        Make use of earthdragon.typelets.inflate.
         """
-        filled = {}
-        _typelets = self._earthdragon_typelets
-
-        if len(args) > len(_typelets):
-            raise InvalidInitInvocation("Passed too many positional values")
-
-        if not set(kwargs).issubset(_typelets):
-            raise InvalidInitInvocation("Pass non typelet keyword arg")
-
-        for arg, name in zip(args, _typelets):
-            fill(self, filled, name, arg)
-
-        for k, v in kwargs.items():
-            fill(self, filled, k, v)
-
-        if set(filled) != set(_typelets):
-            raise InvalidInitInvocation("Need to fill all args")
+        inflate(self, args, kwargs)
